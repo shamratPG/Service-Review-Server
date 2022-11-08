33a6244e9c01a999a4d34f7a3bc350d8fad23223
Md. Shamrat Hossain <shamratpg@gmail.com>
2022-11-08 10:02:07 +0600

    added env

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
 
 app.get('/', (req, res) => {
     res.send('Server is running')
 });
+
+
+
+const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.pra9xm3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+    console.log("database connected")
+});
+
 
 app.use(cors())
 
