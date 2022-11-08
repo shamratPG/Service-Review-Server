a02657611bef83739e584ec3e62f957e9ddd2264
Md. Shamrat Hossain <shamratpg@gmail.com>
2022-11-08 09:38:27 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -6,6 +7,8 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('Server is running')
 });
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`simple node server is running on port: ${port}`)
