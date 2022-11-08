dd5ffef34c1a28a7930d5eeb21617d40862f19c7
Md. Shamrat Hossain <shamratpg@gmail.com>
2022-11-09 00:40:30 +0600

    added service api

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ require('dotenv').config();
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 
+
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Server is running')
 });
@@ -17,15 +21,32 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.pra9xm3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-    console.log("database connected")
-});
+async function run() {
+    try {
+        const serviceCollection = client.db('mrPhotographer').collection('photoServices');
 
+        app.get('/services', async (req, res) => {
+            const size = parseInt(req.query.size);
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(size).toArray();
+            res.send(services);
+        })
 
-app.use(cors())
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log(service)
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run()
+
 
 app.listen(port, () => {
     console.log(`simple node server is running on port: ${port}`)
